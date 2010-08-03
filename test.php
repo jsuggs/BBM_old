1d3e3f7c362d2094d5204fb2cfdf6bc62110b51f
Jonathon Suggs <jsuggs@murmp.com>
2010-08-03 01:07:31 -0500

    Old stuffs

--- test.php
+++ test.php
@@ -5,7 +5,9 @@ set_include_path(realpath(dirname(__FILE__)));
 use BBM\Pitcher,
     BBM\StartingLineup,
     BBM\Team,
-    BBM\Game;
+    BBM\Game,
+    BBM\BettingPlanService,
+    BBM\Strategies\RecentGameStrategy;
 
 $royH = new Pitcher('Roy','Halladay');
 $ccSab = new Pitcher('CC','Sabathia');
@@ -22,7 +24,19 @@ $phi = new Team('NYY','Yankees','AL');
 $phi->setStartingLineup($lineup1);
 $cle->setStartingLineup($lineup2);
 
-$game = new Game($phi,$cle,new DateTime());
+$game1 = new Game($phi,$cle,new DateTime('2010-09-01'));
+$game2 = new Game($phi,$cle,new DateTime('2010-09-02'));
+$game3 = new Game($phi,$cle,new DateTime('2010-09-04'));
+
+// Create a set of games
+$games = array($game1,$game2,$game3);
+$strategies = array(new RecentGameStrategy());
+
+// Return a Betting Plan
+$plan = new BBM\BettingPlanService($games,$strategies);
+
+// Apply the betting plan
+$plan->applyToGames($games);
 
 //echo $royH . "\n";
 
